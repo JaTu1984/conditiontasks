dfe15490a67350c8cf046922cbb19def83b24909
Janne M Tuomela <JaTu1984@gmail.com>
2018-09-26 20:49:18 +0300

    Tasks 3 and 4

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -10,21 +10,34 @@ namespace ConsoleApp3
             Console.WriteLine("Anna ohjelmalle 1 kokonaisluku:");
             luku1 = Int32.Parse(Console.ReadLine());
 
-            if ((luku1 > 0) && ((luku1 % 2) == 0))
+                    if ((luku1 > 0) && ((luku1 % 2) == 0))
 
-                Console.WriteLine("Luku on positiivinen ja parillinen");
+                    Console.WriteLine("Luku on positiivinen ja parillinen");
 
             else
 
-                if ((luku1 < 0) && ((luku1 % 2) == 0));
+                    if ((luku1 < 0) && ((luku1 % 2) == 0))
 
                     Console.WriteLine("Luku on negatiivinen ja parillinen");
 
-                else
+            else
 
-                    if ((luku1 = 0) && ((luku1 % 2) == 0)) ;
+                    if ((luku1 == 0) && ((luku1 % 2) == 0))
 
                     Console.WriteLine("Luku on nolla ja parillinen");
+
+            else
+
+                    if ((luku1 > 0 && ((luku1 % 2 == 1))))
+
+                    Console.WriteLine("Luku on positiivinen ja pariton");
+
+            else
+
+                    if ((luku1 < 0) && ((luku1 % 2) == 1))
+
+                    Console.WriteLine("Luku on negatiivinen ja pariton");
+
 
 
         }
