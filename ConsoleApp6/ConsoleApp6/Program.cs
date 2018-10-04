85a04761f072e2b1b5fe70653b10fed63afb3e93
Janne M Tuomela <JaTu1984@gmail.com>
2018-10-04 08:28:29 +0300

    LoopTask1

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Ohjelma laskee N! kertoman arvon");
+            Console.Write("Anna kokonaisluku: ");
+           
+            
+
+        
+        }
+    }
+}
