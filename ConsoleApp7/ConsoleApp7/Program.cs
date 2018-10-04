4d554542f86f9644c2b188683dd33715ce60b65c
Janne M Tuomela <JaTu1984@gmail.com>
2018-10-04 10:55:46 +0300

    LoopTask 3.3 ready

--- ConsoleApp7/ConsoleApp7/Program.cs
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -6,8 +6,32 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjelma laskee parittomien ja parillisten lukujen summan.");
+            
+            int number = 0;
+            int i = 1;
+            int evenSum = 0;
+            int oddSum = 0;
 
+            Console.WriteLine("Ohjelma laskee parittomien ja parillisten lukujen summan.");
+            Console.Write("Syötä luku: ");
+            number = int.Parse(Console.ReadLine());
+
+            while(i<=number)
+            {
+                if (i%2==0)
+                {
+                    evenSum = evenSum + i; //evenSum += i;
+               
+                }
+                else
+                {
+                    oddSum = oddSum + i;
+                    
+                }
+                i++;
+            }
+            Console.WriteLine($"Parillisten summa on {evenSum}");
+            Console.WriteLine($"Parittomien summa on {oddSum}");
         }
     }
 }
